541309ca9b16371276ea8f56b25bdbea89395145
(minjae8563) <(minjae8563@naver.com)>
2025-04-28 16:18:24 +0900

    chore: 사용하지 않는 import 문 제거

--- src/main/java/roomescape/reservation/JdbcReservationRepository.java
+++ src/main/java/roomescape/reservation/JdbcReservationRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
-import roomescape.dto.ReservationRequest;
 import roomescape.reservationTime.ReservationTime;
 
 public class JdbcReservationRepository implements ReservationRepository {
